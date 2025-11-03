259b32e8ecaf5c81d219eb867ffd0281648e09e1
Varunkasam <varunkasam111@gmail.com>
2025-11-03 10:17:59 +0530

    Day 3

---
+++ E3_maj_min/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pe-rnd3/BBB/MyLKMs/E3_maj_min/Module.symvers :=  sed 's/ko$$/o/'  /home/pe-rnd3/BBB/MyLKMs/E3_maj_min/modules.order | scripts/mod/modpost -m      -o /home/pe-rnd3/BBB/MyLKMs/E3_maj_min/Module.symvers -e -i Module.symvers -T - 
---
+++ E3_maj_min/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pe-rnd3/BBB/MyLKMs/E3_maj_min/modules.order := {   echo /home/pe-rnd3/BBB/MyLKMs/E3_maj_min/E3_maj_min.ko; :; } > /home/pe-rnd3/BBB/MyLKMs/E3_maj_min/modules.order
